5d9dc08af733b82d2d83ea5c9a2b045826b6bb90
Cory Westerfield <cory.westerfield@gmail.com>
2019-04-30 10:01:46 -0600

    Add fetch endpoints to app.js file

--- app.js
+++ app.js
@@ -14,4 +14,40 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(app.get("PORT"));
 
+app.get('/hedgie', function(req, res) {
+  fetch("https://hedgehog-party.herokuapp.com/api/v1/invites")
+  .then((response) => response.json())
+  .then((result) => console.log(result))
+
+  res.send();
+});
+
+app.post('/hedgie', function(req, res) {
+  fetch("https://hedgehog-party.herokuapp.com/api/v1/invites", {
+    method: "POST",
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      name: "Sofie2",
+      hoglets: 3,
+      allergies: "advil"
+    })
+  });
+
+  res.send();
+});
+
+app.delete('/hedgie', function(req, res) {
+  fetch("https://hedgehog-party.herokuapp.com/api/v1/invites/6", {
+    method: "DELETE",
+    headers: { 'Content-Type': 'application/json' }
+  })
+  .then((response) => response.json())
+  .then((result) => console.log(result))
+
+  res.send();
+});
+
+
+
+
 module.exports = app;
